a7d1b67f6f8ceaa2327bc7f2e1596b9d8bd82b02
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-24 15:14:59 +0000

    use fluentci sdk v0.2.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-import Client from "https://sdk.fluentci.io/v0.1.9/mod.ts";
+import Client from "https://sdk.fluentci.io/v0.2.0/mod.ts";
 export default Client;
-export { connect, Container } from "https://sdk.fluentci.io/v0.1.9/mod.ts";
+export { connect, Container } from "https://sdk.fluentci.io/v0.2.0/mod.ts";
